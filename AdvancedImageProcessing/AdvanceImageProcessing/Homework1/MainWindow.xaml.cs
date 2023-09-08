3a227f66a95f4c7986ebf3d70a5ab10bd5f63612
Tigercat <tigercat0828@gmail.com>
2023-09-08 12:12:16 +0800

    update

--- AdvancedImageProcessing/AdvanceImageProcessing/Homework1/MainWindow.xaml.cs
+++ AdvancedImageProcessing/AdvanceImageProcessing/Homework1/MainWindow.xaml.cs
@@ -1,19 +1,13 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using Color = System.Drawing.Color;
+using Image = System.Windows.Controls.Image;
+// rotate image 
 namespace Homework1 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,40 +18,60 @@ namespace Homework1 {
         }
 
         string _fileName;
-        BitmapImage _processedImage;
-        BitmapImage _sourceIamge;
+        Bitmap _processBmp;
+        Bitmap _sourceBmp;
 
         private void OpenFileBtn_Click(object sender, RoutedEventArgs e) {
             var dialog = new OpenFileDialog();
             dialog.Filter = "Supported Image File|*.jpg; *.png; *.ppm; *.bmp";
             if(dialog.ShowDialog() == true) {
-
-                _fileName = dialog.FileName;
                 
-                _processedImage = new BitmapImage(new Uri(dialog.FileName));
-                _sourceIamge = new BitmapImage(new Uri(dialog.FileName));
+                Bitmap srcImg = new Bitmap(dialog.FileName);
 
-                SourceImg.Source = _sourceIamge;
-                ProcessedImg.Source = _processedImage;
+                _processBmp = new Bitmap(srcImg);
+                _sourceBmp = new Bitmap(srcImg);
+
+                SourceImgBox.Source = BitmapToImageSource(srcImg);
+                ProcessedImgBox.Source = BitmapToImageSource(srcImg);
             }
         }
-
+        
         private void SaveFileBtn_Click(object sender, RoutedEventArgs e) {
-
         }
 
         private void RightRotateBtn_Click(object sender, RoutedEventArgs e) {
-
+            RightRoate();
         }
 
         private void LeftRotateBtn_Click(object sender, RoutedEventArgs e) {
-
+            LeftRotate();
+            UpdateImageBox(ProcessedImgBox, _processBmp);
         }
         private void RightRoate() {
 
         }
         private void LeftRotate() {
+            for (int i = 0; i < _processBmp.Width / 2; i++) {
+                for (int j = 0; j < _processBmp.Height / 2; j++) {
+                    _processBmp.SetPixel(i, j, Color.AliceBlue);
+                }
+            }
+        }
 
+        private BitmapImage BitmapToImageSource(Bitmap bitmap) {
+            using (MemoryStream memory = new MemoryStream()) {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                return bitmapimage;
+            }
+        }
+        private void UpdateImageBox(Image imgBox, Bitmap bitmap) {
+            imgBox.Source = BitmapToImageSource(bitmap);
         }
     }
 }
